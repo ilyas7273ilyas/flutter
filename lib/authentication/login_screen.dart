0e35dcc585f68028068f6d4a2c917c221faa5665
ILIYAS ANSARI <ilyasansari620@gmail.com>
2023-12-07 22:47:50 +0530

    completed upto 6th video

---
+++ lib/authentication/login_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:users_app/authentication/signup_screen.dart';
+import 'package:users_app/methods/common_methods.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+{
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  CommonMethods cMethods = CommonMethods();
+
+  checkIfNetworkIsAvailable()
+  {
+    cMethods.checkConnectivity(context);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+
+              Image.asset(
+                  "assets/images/logo.png"
+              ),
+
+              const Text(
+                "Login as a User",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+
+              //Text Fields + Login Button
+              Padding(
+                padding: const EdgeInsets.all(22),
+                child: Column(
+                  children: [
+
+                    TextField(
+                      controller: emailTextEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: "User Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 22,),
+
+                    TextField(
+                      controller: passwordTextEditingController,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 22,),
+
+                    ElevatedButton(
+                      onPressed: ()
+                      {
+                        checkIfNetworkIsAvailable();
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10)
+                      ),
+                      child: const Text(
+                          "Login"
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 12,),
+
+              //Text Buttons
+              TextButton(
+                onPressed: ()
+                {
+                  Navigator.push(context, MaterialPageRoute(builder: (c)=> SignUpScreen()));
+                },
+                child: const Text(
+                  "Don\'t have an Account? Register Here",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/authentication/signup_screen.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:users_app/authentication/login_screen.dart';
+import 'package:users_app/methods/common_methods.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen>
+{
+  TextEditingController userNameTextEditingController = TextEditingController();
+  TextEditingController userPhoneTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  CommonMethods cMethods = CommonMethods();
+
+  checkIfNetworkIsAvailable()
+  {
+    cMethods.checkConnectivity(context);
+
+    signUpFormValidation();
+  }
+
+  signUpFormValidation()
+  {
+    if(userNameTextEditingController.text.trim().length < 4)
+      {
+        cMethods.displaySnackBar("Your Name must be at least 4 or more characters.", context);
+      }
+    else if(userPhoneTextEditingController.text.trim().length < 10)
+    {
+      cMethods.displaySnackBar("Please Enter valid Phone Number.", context);
+    }
+    else if(!emailTextEditingController.text.contains("@"))
+    {
+      cMethods.displaySnackBar("Please write valid Email", context);
+    }
+    else if(passwordTextEditingController.text.trim().length < 6)
+    {
+      cMethods.displaySnackBar("Your Password must be atleast 6 or more characters.", context);
+    }
+    else
+      {
+        //register here
+      }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+
+              Image.asset(
+                  "assets/images/logo.png"
+              ),
+
+              const Text(
+                "Create a User\'s Account",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+
+              //Text Fields + Sign Up Button
+              Padding(
+                padding: const EdgeInsets.all(22),
+                child: Column(
+                  children: [
+
+                    TextField(
+                      controller: userNameTextEditingController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Name",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 22,),
+
+                    TextField(
+                      controller: userPhoneTextEditingController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Phone Number",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 22,),
+
+                    TextField(
+                      controller: emailTextEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: "User Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 22,),
+
+                    TextField(
+                      controller: passwordTextEditingController,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: "User Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                    const SizedBox(height: 22,),
+
+                    ElevatedButton(
+                        onPressed: ()
+                      {
+                        checkIfNetworkIsAvailable();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10)
+                      ),
+                      child: const Text(
+                        "Sign Up"
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 12,),
+
+              //Text Buttons
+              TextButton(
+                onPressed: ()
+                {
+                  Navigator.push(context, MaterialPageRoute(builder: (c)=> LoginScreen()));
+                },
+                child: const Text(
+                  "Already have an Account? Login Here",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
